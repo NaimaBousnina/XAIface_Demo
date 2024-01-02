36bb95c85b0a363d8b730364f62a174313f243d4
NaiLa <41441592+NaimaBousnina@users.noreply.github.com>
2024-01-02 11:56:50 +0000

    Update welcome_page.py

--- welcome_page.py
+++ welcome_page.py
@@ -27,8 +27,8 @@ st.markdown("""
     display: block;
     margin-top: -5px;
     width: 100%;
-    margin-left: 500px;
-    margin-right: 50px;
+    margin-left: 100px;
+    margin-right: 100px;
 
 }
 
