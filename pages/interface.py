3319dc381c4226a9e69b0727898c90599f2b0162
Naima <41441592+NaimaBousnina@users.noreply.github.com>
2024-03-04 16:07:46 +0000

    a

--- pages/interface.py
+++ pages/interface.py
@@ -176,10 +176,10 @@ with first_container:
         contain_3_2 = st.container()
 
         with contain_3_1:
-            st.markdown("<h6 style='margin-left: 20px; margin-right: 20px; margin-top: -20px; background-color:#E0FFFF; border-radius: 10px 10px; height: 120px; outline: 2px solid #AFEEEE;'><p style='margin-top: 10px; text-align: justify;margin-left: 15px; margin-right: 15px; font-size:21px; color: Black;'>Probe Image:</p> <p>Gender:</p> <p>Age:</p> <p> Weight:</p> </h6>", unsafe_allow_html=True)
+            st.markdown("<h6 style='margin-left: 20px; margin-right: 20px; margin-top: -20px; background-color:#E0FFFF; border-radius: 10px 10px; height: 140px; outline: 2px solid #AFEEEE;'><p style='margin-top: 10px; text-align: justify;margin-left: 15px; margin-right: 15px; font-size:21px; color: Black;'>Probe Image:</p> <p>Gender:</p> <p>Age:</p> <p> Weight:</p> </h6>", unsafe_allow_html=True)
 
         with contain_3_2:
-            st.markdown("<h6 style='margin-left: 20px; margin-right: 20px; margin-top: 30px; background-color:#E0FFFF; border-radius: 10px 10px; height: 120px; outline: 2px solid #AFEEEE;'><p style='margin-top: 10px; text-align: justify;margin-left: 15px; margin-right: 15px; font-size:21px; color: Black;'>Gallery Image:</p></h6>", unsafe_allow_html=True)
+            st.markdown("<h6 style='margin-left: 20px; margin-right: 20px; margin-top: 30px; background-color:#E0FFFF; border-radius: 10px 10px; height: 140px; outline: 2px solid #AFEEEE;'><p style='margin-top: 10px; text-align: justify;margin-left: 15px; margin-right: 15px; font-size:21px; color: Black;'>Gallery Image:</p></h6>", unsafe_allow_html=True)
 
  
 with second_container:
